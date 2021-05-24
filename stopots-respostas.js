1fbfd7a6a06a505e2de0ceb9a320dc89dc5cc7d4
Alielson Piffer <alielson.piffer@industriall.ai>
2021-05-24 17:13:44 -0300

    .

--- stopots-respostas.js
+++ stopots-respostas.js
@@ -1,3 +1,22 @@
-function teste() {
-  alert("oi");
+function responder() {
+  var letra = document.querySelector("#letter span").textContent;
+
+  console.log(letra);
+
+  document.querySelectorAll(".answers label").forEach((e, i, l) => {
+    var tema = e.getElementsByTagName(span).item(0).textContent;
+    console.log(tema);
+
+    var input = e.getElementsByTagName(input).item(0);
+    console.log(input.innerHtml);
+
+    input.text = getAnswer(letra, tema);
+  });
+
+  console.log("fim");
+}
+
+function getAnswer(letra, tema) {
+  console.log(`pegando resposta: ${letra} - ${tema}`);
+  return "teste";
 }
